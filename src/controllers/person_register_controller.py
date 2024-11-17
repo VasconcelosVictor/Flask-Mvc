5472d53b0d850c03477dbe7bdefad5358d69ec59
VasconcelosVictor <victorvasconcelos6x@hotmail.com>
2024-11-17 01:43:54 -0400

    feature: adicionando diretorio models

--- src/controllers/person_register_controller.py
+++ src/controllers/person_register_controller.py
@@ -1,4 +1,8 @@
+from src.models.db.repositores.peaple_repository import PeapleRepository
 class PersonRegisterController:
+    def __init__(self, ) -> None:
+        self.__person_repository = PeapleRepository()
+
     def create_person(self, person_name :str, person_age : int ) -> dict:
         self.__validate_person_registry(person_name)
         self.__insert_person(person_name, person_age)
@@ -7,12 +11,12 @@ class PersonRegisterController:
 
     
     def __validate_person_registry(self, person_name : str) -> None:
-        person = "" # Buscar em banco o registro
+        person = self.__person_repository.get_person_by_name(person_name)
         if person:
             raise Exception("Person already registered")
 
     def __insert_person(self, person_name : str, person_age : int) -> None:
-        pass
+        self.__person_repository.registry_person(person_name, person_age)
 
     def __format_response(self, person_name : str) -> dict:
         return {
